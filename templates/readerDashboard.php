ebd117de3fb3c31879887092447d7e6620a8a44e
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-01-02 16:53:38 +0100

    Enhanced UI for reader pages

--- templates/readerDashboard.php
+++ templates/readerDashboard.php
@@ -14,8 +14,48 @@
 </head>
 <body class="bg-gray-100">
 
+
+<button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+   <span class="sr-only">Open sidebar</span>
+   <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+   </svg>
+</button>
+
+<aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-80 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+    <div class="h-full overflow-y-auto bg-black">
+    <!-- Sidebar Menu -->
+    <div class="flex flex-col">
+        <img src="../assets/theatre.jpg" alt="Cover" class="object-cover">
+        <div class="px-3 py-4">
+            <h2 class="text-3xl font-semibold text-center text-white mb-6">Artflex</h2>
+            <hr class="h-1 bg-white border-0 rounded dark:bg-gray-400">
+        </div>
+    </div>
+        <ul class="space-y-2 font-medium px-3 pb-4">
+            <li>
+                <a href="#" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-100 hover:text-black group">
+                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+                    </svg>
+                <span class="ms-3">Filtre by:</span>
+                </a>
+            </li>
+            <li>
+                <a href="logout.php" class="flex items-center p-2 text-white rounded-lg hover:bg-gray-100 hover:text-black group">
+                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
+                </svg>
+                <span class="flex-1 ms-3 whitespace-nowrap">Log Out</span>
+                </a>
+            </li>
+        </ul>
+   </div>
+</aside>
+
 <!-- Main -->
-<div class="flex-1 p-8">
+<div class="flex-1 p-8 sm:ml-80">
     <h2 class="text-4xl font-semibold text-black mb-10">Find & Book your Activity</h2>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4" style="align-items: start;">
@@ -89,7 +129,7 @@
 </div>
 
 
-<footer class="bg-gray-100">
+<footer class="bg-gray-100 sm:ml-80">
   <div class="mx-auto max-w-5xl px-4 py-16 sm:px-6 lg:px-8">
     <div class="flex justify-center text-teal-600">
         <img src="../assets/art.png" class="w-40 h-auto">
